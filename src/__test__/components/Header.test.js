55b3a0d03da2446ac6638e0a7bca383a50fd4d1a
Federico Leiva <federicoleiva@MBP-de-Federico.fibertel.com.ar>
2021-05-17 15:09:33 -0300

    first commit

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+  it('should render', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.length).toEqual(1);
+  });
+
+  it('should render the title', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('should render correctly', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+});
